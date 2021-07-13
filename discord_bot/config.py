e9923041125a997afdf9bd6f17ce669f980ba2f6
Chirag-droid <chirag.singla.pi@gmail.com>
2021-07-13 13:58:39 +0530

    Removed envyaml dependency and implement custom yaml loader

--- discord_bot/config.py
+++ discord_bot/config.py
@@ -1,12 +1,37 @@
+import os
+import re
 from os.path import exists
 from typing import NamedTuple
 
-from envyaml import EnvYAML
+import yaml
+
+
+def _constructor_env_variables(loader, node):
+    pattern = re.compile(r".*?\${(\w+)}.*?")
+    value = loader.construct_scalar(node)
+    match = pattern.findall(value)
+    if match:
+        full_value = value
+        for g in match:
+            full_value = full_value.replace(f"${{{g}}}", os.environ.get(g, g))
+        return full_value
+    return value
+
+
+def _construct_yaml_tuple(self, node):
+    seq = self.construct_sequence(node)
+    return tuple(seq)
+
+
+yaml.SafeLoader.add_constructor("tag:yaml.org,2002:python/tuple", _construct_yaml_tuple)
+yaml.SafeLoader.add_constructor("!ENV", _constructor_env_variables)
 
 if not exists("config.yml"):
-    config = EnvYAML("ex_config.yml")
+    with open("ex_config.yml") as f:
+        config = yaml.safe_load(f)
 else:
-    config = EnvYAML("config.yml")
+    with open("config.yml") as f:
+        config = yaml.safe_load(f)
 
 
 class BotConfig(NamedTuple):
